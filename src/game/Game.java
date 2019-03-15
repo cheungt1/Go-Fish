dd16079c3e672d7d9a0c960f1595e1115837a5b8
cheungt1 <cheungt1@WIT45001318.wit.private>
2019-03-15 14:59:47 -0400

    java version change?

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,40 +2,55 @@ package game;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 
 public class Game {
-	private int[] deck = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13,
-						 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 ,25, 26,
-						 27, 28 ,29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 
-						 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
+	private int[] deck = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
+						  14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 
+						  27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 
+						  40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52 };
 	private Queue<Integer> order = new LinkedList<Integer>();
 	private static int numPlayer = 1;
-	
+
 	public Game() {
 	}
-	
+
 	public void start() {
 		shuffle();
 		initialiseHand();
 	}
-	
+
 	public void join() {
 		order.add(numPlayer);
 		numPlayer++;
 	}
+
 	private int playerTurn() {
 		Integer first = order.remove();
-		order.add(first);
 		
+		order.add(first);
+
 		return first;
 	}
-	
+
 	private void shuffle() {
-		
+		Random rnd = new Random();
+
+		for (int i = 0; i < deck.length; i++) {
+			/*
+			 * range of [0,51]
+			 * swapping cards between current location and a random location
+			 */
+			int n = rnd.nextInt(deck.length);
+
+			int temp = deck[n];
+			deck[n] = deck[i];
+			deck[i] = temp;
+		}
 	}
-	
+
 	private void initialiseHand() {
-		
+
 	}
-	
+
 }
