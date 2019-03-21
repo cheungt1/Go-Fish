4ca8951ca6c4216b1f3ebb9f243854b39eeef619
cheungt1 <cheungt1@WIT45001318.wit.private>
2019-03-21 12:00:16 -0400

    added a method to check score

--- src/game/Method_Test.java
+++ src/game/Method_Test.java
@@ -1,21 +1,10 @@
 package game;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Method_Test {
 	public static void main(String[] args) {
-		Game.shuffle();
-		Player a = new Player("john");
-		Object[] aHand = a.getHand().toArray();
 		
-		Player b = new Player("doe");
-		Object[] bHand = b.getHand().toArray();
-		
-		System.out.println(a.toString() +"\n");
-		for(int i = 0; i < aHand.length; i++) {
-			System.out.println(aHand[i]);
-		}
-		System.out.println(b.toString()+"\n");
-		for(int i = 0; i < bHand.length; i++) {
-			System.out.println(bHand[i]);
-		}
 	}
 }
--- src/game/Player.java
+++ src/game/Player.java
@@ -1,10 +1,11 @@
 package game;
 
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class Player {
 	private String name;
+	private int score;
 	private LinkedList<Integer> hand = new LinkedList<Integer>();
 	
 	public Player(String name) {
@@ -17,6 +18,26 @@ public class Player {
 	public boolean containCard(Integer x) {
 		return hand.contains(x%13);
 	}
+	
+	public void cardCheck() {		
+		Collections.sort(hand);
+		
+		Integer current = hand.getFirst();
+		Integer count = 1;
+		
+		for(int i = 1; i < hand.size(); i++) {			
+			if(hand.get(i) == current) {
+				count++;
+				if(count == 4) {
+					score++;
+				}
+			}
+			else {
+				current = hand.get(i);
+				count = 1;
+			}
+		}	
+	}		
 	
 	public LinkedList<Integer> getHand(){
 		return hand;
