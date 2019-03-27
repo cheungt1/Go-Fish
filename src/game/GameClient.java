650e6059728e08fb4bca2b76b8517cfae81b5ebf
cheungt1 <cheungt1@WIT45001318.wit.private>
2019-03-27 12:29:02 -0400

    added a while loop to the client

--- src/game/GameClient.java
+++ src/game/GameClient.java
@@ -8,30 +8,35 @@ import static game.Util.*;
 public class GameClient {
 	private final static String serverHostIP = "";
 	private final static int port = 0;
-	
+
 	public static void main(String[] args) {
 		try {
 			Socket socket = new Socket(serverHostIP, port);
-			
+
 			DataInputStream is = new DataInputStream(socket.getInputStream());
 			DataOutputStream os = new DataOutputStream(socket.getOutputStream());
-			
+
 			// send player name to server
-			writeWithThread(os,GUI.getUserName());
-			
+			writeWithThread(os, GUI.getUserName());
+
 			Player you = new Player(GUI.getUserName());
-			
-			while(Game.matched != 13) {
-				if(Game.playerTurn() == you.getPlayerNum()) {
-					String playerChoice = GUI.getPlayerChoice();
-					int card = Integer.parseInt(GUI.getCardValue());
-					
-					String selection = playerChoice + " " + card;
-					
-					writeWithThread(os, selection);
+
+			while (Game.matched != 13) {
+				if (Game.playerTurn() == you.getPlayerNum()) {
+					int cardsRec = 0;
+					do {
+						String playerChoice = GUI.getPlayerChoice();
+						int card = Integer.parseInt(GUI.getCardValue());
+
+						String selection = playerChoice + " " + card;
+
+						writeWithThread(os, selection);
+
+						cardsRec = is.readInt();
+					} while (cardsRec != 0);
 				}
 			}
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
