6158a409126dbe8667b30c760381aa3cf52c68c9
zilongyang <yangz1@wit.edu>
2019-03-29 11:40:26 -0400

    Added Reader class in Util.

--- src/game/Util.java
+++ src/game/Util.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
@@ -15,5 +16,41 @@ public class Util {
                 e.printStackTrace();
             }
         }).start();
+    }
+
+    /**
+     * A Runnable that serves as a reader that reads and prints
+     * continuously from the given input stream. This class is meant
+     * to be used for a thread.
+     */
+    public static class Reader implements Runnable {
+
+        // the input stream to be read from
+        private DataInputStream is;
+
+        /**
+         * Create a new Reader.
+         *
+         * @param is the data input stream
+         */
+        public Reader(DataInputStream is) {
+            this.is = is;
+        }
+
+        @Override
+        public synchronized void run() {
+            try {
+                //noinspection InfiniteLoopStatement
+                while (true) {
+                    if (is.available() > 0) { // if the stream has more bytes
+                        String s = is.readUTF(); // read one token
+                        System.out.println(s); // print the token
+                    }
+                }
+            } catch (IOException ioe) {
+                System.err.println("Reading Error");
+                ioe.printStackTrace();
+            }
+        }
     }
 }
